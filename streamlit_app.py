ba7440ed0dbeff156a6bfd643c09144247ab4d2f
GouthamKKamisetty <150376290+GouthamKKamisetty@users.noreply.github.com>
2023-11-10 07:42:33 +0530

    Update streamlit_app.py
    added new items and icons as well for all items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit
 streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 & Blueberry OatMeal")
-streamlit.text("Kale , SPinach Rocket Smoothie")
-streamlit.text("Hard Boiled Free range eggs")
+streamlit.text("🥣 Omega 3 & Blueberry OatMeal")
+streamlit.text("🥗 Kale , SPinach Rocket Smoothie")
+streamlit.text("🐔 Hard Boiled Free range eggs")
+streamlit.text("🥑🍞 Avacado Toast")
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
